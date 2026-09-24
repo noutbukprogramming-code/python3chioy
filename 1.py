b6a4d46fe4cf9fcb7b6e3cd4ba32f1d29dd885e5
chaqimchi-git <noutbukprogramming@gmail.com>
2026-09-24 09:41:38 +0500

    1chi dars

---
+++ 1.py
@@ -0,0 +1,7 @@
+ism = input("Ismingizni kiriting: ");
+familiya = input("Familiyangizni kiriting: ");
+shaxar = input("Shaxaringizni kiriting: ");
+
+print("\nIsmizgiz: ", ism);
+print("Familiyangiz: ", familiya);
+print("Shaxaringiz: ", shaxar);
